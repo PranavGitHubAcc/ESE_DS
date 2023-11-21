87d0059f02de595b292642632d743a316a076fe8
Pranav Mahajan <pranavmahajan18@gmail.com>
2023-11-21 09:28:29 +0530

    Update TREE_codes.c

--- TREE_codes.c
+++ TREE_codes.c
@@ -1,1 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct Node{
+    int data;
+    struct Node *left;
+    struct Node *right;
+}Node;
+
+Node* create(int data){
+    Node *newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+Node* insert(Node *root, int data){
+    if(root == NULL){
+        return create(data);
+    }
+    if(data < root->data){
+        root->left = insert(root->left, data);
+    }
+    else if(data > root->data){
+        root->right = insert(root->right, data);
+    }
+    return root;
+}
+
+Node *search(Node* root, int data){
+    if(root==NULL){
+        printf("Element not found");
+        return root;
+    }
+    if(data < root->data ){
+        return search(root->left, data);
+    }else if(data > root->data){
+        return search(root->right, data);
+    }else{
+        printf("Element Found");
+        return root;
+    }
+}
+
+int findMin(Node *root){
+    while(root->left!=NULL){
+        root = root->left;
+    }
+    return root->data;
+}
+
+Node* deleteNode(Node *root, int data){
+    if(root == NULL){
+        return root;
+    }
+    if(data < root->data){
+        root->left = deleteNode(root->left, data);
+    }
+    else if(data > root->data){
+        root->right = deleteNode(root->right, data);
+    }
+    else{
+        if(root->left == NULL){
+            return root->right;
+        }else if(root->right == NULL){
+            return root->left;
+        }
+        root->data = findMin(root->right);
+        root->right = deleteNode(root->right, root->data);
+    }
+    return root;
+}
+
+int searchItter(Node *root, int data){
+    while(root!=NULL){
+        if(root->data == data){
+            printf("Element found");
+            return 1;
+        }
+        if(data < root->data ){
+            root = root->left;
+        }else{
+            root = root->right;
+        }
+    }
+    printf("Element not found G.");
+    return 0;
+}
+
+void inorder(Node* root){
+    if(root!=NULL){
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+int main(){
+    Node *root = create(10);
+    insert(root, 20);
+    insert(root, 15);
+    insert(root, 25);
+    insert(root, 18);
+    insert(root, 22);
+    insert(root, 14);
+    search(root, 20);
+    inorder(root);
+    deleteNode(root,20);
+    inorder(root);
+}
